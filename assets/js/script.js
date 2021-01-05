9208ddea00308b0de94fe840dad4393b56d77687
mdheerajprdxn <dheerajm@prdxn.com>
2021-01-05 13:52:50 +0530

    [js] main done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 let form = document.querySelector("form");
 
+// import { showData } from "./showData.js";
+
 let submitForm = (e) => {
   e.preventDefault();
   let key = e.target.querySelector("#key").value;
@@ -15,25 +17,28 @@ let submitForm = (e) => {
 form.addEventListener("submit", submitForm);
 
 let dataTR = document.querySelector("table");
+
 let showData = () => {
   console.log(localStorage);
+  console.log("hi");
   dataTR.innerHTML = ` <tr>
-    <th>Key</th>
-    <th>Value</th>
-  </tr>`;
+      <th>Key</th>
+      <th>Value</th>
+    </tr>`;
   for (const [key, value] of Object.entries(localStorage)) {
     console.log(key, value);
     let data = document.createElement("tr"); // Create a <button> element
     data.innerHTML = `<td>${key} </td> <td> ${value} </td> <td> 
-    <button id=${key} onclick='deleteItem(event)'>delete</button> </td>`; // Insert text
+      <button id=${key} onclick='deleteItem(event)'>delete</button> </td>`; // Insert text
     dataTR.appendChild(data);
   }
   console.log("hi");
 };
-showData();
 
 let deleteItem = (e) => {
   let id = e.target.id;
   delete localStorage[id];
   showData();
 };
+
+showData();
